743f50fe28efd191019e8b2e24c959eee0594d01
hendraet <hendrik.raetz@niederjahna.de>
2018-11-01 18:36:54 +0100

    Adds 2018 geolocation set + lat/long csv converter

--- gps_to_neighborhood.py
+++ gps_to_neighborhood.py
@@ -1,45 +1,49 @@
+import argparse
 import json
 import os
 from collections import namedtuple
 import sys
-from math import log,tan,pi,radians
+from math import log, tan, pi, radians
 
-#globals
-Pt = namedtuple('Pt','x,y')
-Edge = namedtuple('Edge','a,b')
-Poly = namedtuple('Poly','name,edges')
+# globals
+Pt = namedtuple('Pt', 'x,y')
+Edge = namedtuple('Edge', 'a,b')
+Poly = namedtuple('Poly', 'id,name,edges')
 _eps = 1e-5
 _huge = sys.float_info.max
 _tiny = sys.float_info.min
+# neighborhood_json = "Neighborhoods_2012_polygons.json"
+neighborhood_json = "neighborhoods_2018.json"
+
 
 def load_json():
-	file_in = open(os.getcwd()+"/Neighborhoods_2012_polygons.json")
-	d = json.load(file_in)
-	return d
+    file_in = open(os.getcwd() + "/" + neighborhood_json)
+    d = json.load(file_in)
+    return d
 
 
-def spherical_mercator_projection(longitude,latitude):
-	#http://en.wikipedia.org/wiki/Mercator_projection <- invented in 1569!
-	#http://stackoverflow.com/questions/4287780/detecting-whether-a-gps-coordinate-falls-within-a-polygon-on-a-map
-	x = -longitude
-	y = log(tan(radians(pi/4 + latitude/2)))
-	return (x,y)
+def spherical_mercator_projection(longitude, latitude):
+    # http://en.wikipedia.org/wiki/Mercator_projection <- invented in 1569!
+    # http://stackoverflow.com/questions/4287780/detecting-whether-a-gps-coordinate-falls-within-a-polygon-on-a-map
+    x = -longitude
+    y = log(tan(radians(pi / 4 + latitude / 2)))
+    return (x, y)
 
 
 def rayintersectseg(p, edge):
-	#http://rosettacode.org/wiki/Ray-casting_algorithm#Python
-	#takes a point p=Pt() and an edge of two endpoints a,b=Pt() of a line segment returns boolean
-    a,b = edge
+    # http://rosettacode.org/wiki/Ray-casting_algorithm#Python
+    # takes a point p=Pt() and an edge of two endpoints a,b=Pt() of a line segment returns boolean
+    a, b = edge
     if a.y > b.y:
-        a,b = b,a
+        a, b = b, a
     if p.y == a.y or p.y == b.y:
         p = Pt(p.x, p.y + _eps)
     intersect = False
- 
+
     if (p.y > b.y or p.y < a.y) or (
-        p.x > max(a.x, b.x)):
+            p.x > max(a.x, b.x)):
         return False
- 
+
     if p.x < min(a.x, b.x):
         intersect = True
     else:
@@ -51,54 +55,64 @@ def rayintersectseg(p, edge):
             m_blue = (p.y - a.y) / float(p.x - a.x)
         else:
             m_blue = _huge
-        
+
         intersect = m_blue >= m_red
     return intersect
- 
-def is_odd(x): 
-	return x%2 == 1
+
+
+def is_odd(x):
+    return x % 2 == 1
+
 
 def ispointinside(p, poly):
     ln = len(poly)
     return is_odd(sum(rayintersectseg(p, edge)
-                    for edge in poly.edges ))
+                      for edge in poly.edges))
 
-def get_all_neighborhoods():
-	d = load_json()
-	shape_list=[]
-	for shape_idx in range(len(d['features'])):
-		name = d['features'][shape_idx]['properties']['SEC_NEIGH']
 
-		edges =[]
-		for coordinate_idx in range(len(d['features'][shape_idx]['geometry']['coordinates'][0])-1):
-			lon_1 = d['features'][shape_idx]['geometry']['coordinates'][0][coordinate_idx][0]
-			lat_1 = d['features'][shape_idx]['geometry']['coordinates'][0][coordinate_idx][1]
-			
-			lon_2 = d['features'][shape_idx]['geometry']['coordinates'][0][coordinate_idx+1][0]
-			lat_2 = d['features'][shape_idx]['geometry']['coordinates'][0][coordinate_idx+1][1]
-			
-			x1,y1 = spherical_mercator_projection(lon_1,lat_1)
-			x2,y2 = spherical_mercator_projection(lon_2,lat_2)
-			edges.append(Edge(a=Pt(x=x1,y=y1),b=Pt(x=x2,y=y2)))
-		
-		shape_list.append(Poly(name=name,edges=tuple(edges)))
-	return shape_list
+def get_all_neighbourhoods():
+    d = load_json()
+    shape_list = []
+    for shape_idx in range(len(d['features'])):
+        # name = d['features'][shape_idx]['properties']['SEC_NEIGH']
+        name = d['features'][shape_idx]['properties']['community']
+        id = d['features'][shape_idx]['properties']['area_numbe']
 
-def find_neighborhood(test_long,test_lat,all_neighborhoods):
-	x,y = spherical_mercator_projection(test_long,test_lat)
-	for neighborhood in all_neighborhoods:
-		correct_neighborhood = ispointinside(Pt(x=x,y=y),neighborhood)
-		if correct_neighborhood:
-			return neighborhood.name
+        edges = []
+        neighborhood_coordinates = d['features'][shape_idx]['geometry']['coordinates'][0][0]
+        for coordinate_idx in range(len(neighborhood_coordinates) - 1):
+            lon_1 = neighborhood_coordinates[coordinate_idx][0]
+            lat_1 = neighborhood_coordinates[coordinate_idx][1]
 
-all_neighborhoods = get_all_neighborhoods()
+            lon_2 = neighborhood_coordinates[coordinate_idx + 1][0]
+            lat_2 = neighborhood_coordinates[coordinate_idx + 1][1]
 
-test_long = -87.624069
-test_lat = 41.862889
+            x1, y1 = spherical_mercator_projection(lon_1, lat_1)
+            x2, y2 = spherical_mercator_projection(lon_2, lat_2)
+            edges.append(Edge(a=Pt(x=x1, y=y1), b=Pt(x=x2, y=y2)))
 
-neighborhood = find_neighborhood(test_long,test_lat,all_neighborhoods)
-print(neighborhood)
+        shape_list.append(Poly(id=id, name=name, edges=tuple(edges)))
+    return shape_list
 
+
+def find_neighbourhood(lat, long, neighborhoods):
+    x, y = spherical_mercator_projection(long, lat)
+    for neighborhood in neighborhoods:
+        correct_neighborhood = ispointinside(Pt(x=x, y=y), neighborhood)
+        if correct_neighborhood:
+            # return neighborhood.name
+            return neighborhood.id, neighborhood.name
+    return None, None
+
+
+if __name__ == "__main__":
+    all_neighborhoods = get_all_neighbourhoods()
+
+    test_long = -87.73269978462665
+    test_lat = 41.913654239126494
+
+    neighborhood = find_neighbourhood(test_lat, test_long, all_neighborhoods)
+    print(neighborhood)
 
 '''
 #example of iterating through a list of neighborhoods
@@ -128,4 +142,3 @@ for line in file_in:
 file_in.close()
 file_out.close()
 '''
-
---
+++ lat_long_converter.py
@@ -0,0 +1,57 @@
+import argparse
+import csv
+
+import os
+
+from gps_to_neighborhood import get_all_neighbourhoods, find_neighbourhood
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f")
+    args = parser.parse_args()
+    print(args.f)
+    filename = args.f
+    new_filename = os.path.splitext(filename)[0] + "_new" + os.path.splitext(filename)[1]
+
+    with open(filename, "r") as csvfile_in, open(new_filename, "w+") as csvfile_out:
+        reader = csv.DictReader(csvfile_in)
+        reader.fieldnames = [field.lower() for field in reader.fieldnames]
+        fieldnames = reader.fieldnames
+        if "community area name" not in fieldnames:
+            fieldnames.append("community area name")
+        if "community area number" not in fieldnames:
+            fieldnames.append("community area number")
+        print(fieldnames)
+
+        writer = csv.DictWriter(csvfile_out, fieldnames)
+        writer.writeheader()
+
+        # true_count = 0
+        # false_names = []
+
+        neighbourhoods = get_all_neighbourhoods()
+        for (i, row) in enumerate(reader):
+            if i % 1000 == 0:
+                print(str(i) + " done")
+
+            if row["latitude"] and row["longitude"]:
+                lat = float(row["latitude"])
+                long = float(row["longitude"])
+
+                calculated_area_id, calculated_area_name = find_neighbourhood(lat, long, neighbourhoods)
+                calculated_area_name = calculated_area_name
+
+                # area_name = row["Community Area Name"].lower()
+                # area_id = row["Community Area Number"]
+                # if calculated_area_id == area_id:
+                #     true_count += 1
+                # else:
+                #     false_names.append((i, calculated_area_id, area_id))
+
+                row["community area name"] = calculated_area_name
+                row["community area number"] = calculated_area_id
+            writer.writerow(row)
+
+        # print(true_count)
+        # print(false_names)
+
